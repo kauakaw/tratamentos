421a93246fdd7cbef24092e439d1dfe73539ae13
Kaua <kaua.lade14@gmail.com>
2025-03-13 16:01:43 -0300

    teste

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Documentação do app  - Tratamento de Erro
+
+## Introdução 
+
+Este código em python na versão 3.12, demonstra tratamento de erros e também laço de repetição.     
+
+## Propiedade 
+
+Este código foi desenvolvido por: **Kauã** <br>
+Orientado pelo professor: **Thiago** <br>
+Local: *Senai ""Suzana Dias"* <br>
+Data:  *13 de Março de 2025* <br>
+
+## Entre em contato
+
+Para saber mais acesse: [Mcdonalds](https://www.mcdonalds.com.br/)
---
+++ tratamentoErro.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        n1 = int(input("Digite um número: "))
+        n2 = int(input("Digite outro número: "))
+
+        conta = n1 / n2
+
+    except ZeroDivisionError:
+        print("Não é possivel dividir nada por zero! TENTE NOVEMENTE")
+    except ValueError:
+        print("Não é possivel dividir por letras ou números decimais! TENTE NOVAMENTE")
+    else:
+        print(f'{n1} dividido por {n2} é = {round(conta, 2)}')
+        break
